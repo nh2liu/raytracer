5e849d5647b81f8dc6b5c0fa6a9a73834c7c53ea
Neil Liu <neil.liu16@gmail.com>
2017-07-28 01:22:28 -0400

    first raytrace

---
+++ src/camera.h
@@ -0,0 +1,12 @@
+#ifndef _CAMERA_H_
+#define _CAMERA_H_
+#include "vec3.h"
+
+class Camera {
+private:
+
+public:
+  Camera() {}
+};
+
+#endif
--- src/main.cc
+++ src/main.cc
@@ -5,7 +5,11 @@
 #include "ray.h"
 using namespace std;
 
-
+Pixel color(const Ray & r) {
+  Vec3 d_unit = r.direction().unit();
+  double t = 0.5 * (d_unit.y() + 1.0);
+  return (1.0-t) * Pixel(1.0, 1.0, 1.0) + t * Pixel(0.5, 0.7, 1.0);
+}
 
 int main(int argc, char ** argv) {
   int x = 800;
@@ -21,14 +25,17 @@ int main(int argc, char ** argv) {
   ofs.open((path + fileName).c_str());
 
   ofs << "P3" << endl << x << ' ' << y << endl << 255 << endl;
-
+  Vec3 lowerLeftCorner(-2.0, -1.0, -1.0);
+  Vec3 horizontal(4.0, 0.0, 0.0);
+  Vec3 vertical(0, 2.0, 0.0);
+  Vec3 origin(0.0, 0.0, 0.0);
   // code convention is written from top to bottom
   for (int j = y - 1; j >= 0; j--) {
     for (int i = 0; i < x; i++) {
-      float r = float(i) / float(x);
-      float g = float(j) / float(y);
-      float b = 0.2;
-      Pixel pxl(r, g, b);
+      double u = double(i) / double(x);
+      double d = double(j) / double(y);
+      Ray r(origin, lowerLeftCorner + horizontal * u + vertical * d);
+      Pixel pxl = color(r);
       ofs << pxl;
     }
   }
