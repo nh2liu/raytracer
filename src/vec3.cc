965f17da104b60063d121955c9e4478028b706e9
Neil Liu <neil.liu16@gmail.com>
2017-07-30 03:14:30 -0400

    added subtract

--- src/vec3.cc
+++ src/vec3.cc
@@ -20,6 +20,11 @@ Vec3 Vec3::unit() {
               v1.z() + v2.z());
 }
 
+Vec3 operator-(const Vec3 & v1, const Vec3 & v2) {
+ return Vec3(v1.x() - v2.x(),
+             v1.y() - v2.y(),
+             v1.z() - v2.z());
+}
  Vec3 operator*(const Vec3 & v1, const Vec3 & v2) {
   return Vec3(v1.x() * v2.x(),
               v1.y() * v2.y(),
--- src/vec3.h
+++ src/vec3.h
@@ -22,6 +22,7 @@ public:
 };
 
 Vec3 operator+(const Vec3 & v1, const Vec3 & v2);
+Vec3 operator-(const Vec3 & v1, const Vec3 & v2);
 Vec3 operator*(const Vec3 & v1, const Vec3 & v2);
 
 Vec3 operator/(const Vec3 & v1, double c);
